fee67271d983eda91308d99f699f12204c63f626
Michael Festus <micfestus45@gmail.com>
2022-03-09 04:27:18 +0100

    added more stlyes

--- src/App.js
+++ src/App.js
@@ -5,29 +5,25 @@ import Data from './data.js';
 
 
 function App() {
-      const cards = Data.map(data => {
-  return(
-  <Card 
-    title={data.title}
-    image={data.coverImg}
-    rating={data.stats.rating}
-    reviewCount={data.stats.reviewCount}
-    location={data.location}
-      price={data.price}
-      icon= 'Star 1.png'
-    />
-  )
-      })
-  
-      return (
-        <div className="App">
-          <Navbar />
-          <Hero />
-          <section className='card-list'>
-            {cards}
-          </section>
-          </div>
-      );
-    }
+  const cards = Data.map(data => {
+    return (
+      <Card
+        key={data.id}
+        data={data}
+        icon='Star 1.png'
+      />
+    )
+  })
+
+  return (
+    <div className="App">
+      <Navbar />
+      <Hero />
+      <section className='card-list'>
+        {cards}
+      </section>
+    </div>
+  );
+}
 
 export default App;
--- src/Card.js
+++ src/Card.js
@@ -1,19 +1,26 @@
 
 
 const Card = (props) => {
-    console.log(props);
-    return (
+    let badgeText = '';
+    if (props.data.openSpots === 0) {
+        badgeText = 'SOLD OUT'
+    } else if (props.data.location === 'Online') {
+        badgeText = 'ONLINE'
+    }
+    return (     
+        
         <section className='card'>
-            <img alt='Cover img' src={props.image} className='card-image' />
+            {badgeText && <div className="badge">{badgeText}</div>}
+            <img alt='Cover img' src={props.data.coverImg} className='card-image' />
 
             <div className='card--stats'>
                 <img alt='star icon' src={props.icon} className='card--star' />
-                <span>{props.rating}</span>
-                <span className='gray'>{props.reviewCount}</span>
-                <span className='gray'>{props.location}</span>
+                <span>{props.data.stats.rating}</span>
+                <span className='gray'>({props.data.stats.reviewCount}) &bull;</span>
+                <span className='gray'>{props.data.location}</span>
             </div>
-            <p className="card--title">{props.title}</p>
-            <p className="card--price"><span className='bold'>${props.price} </span>/ person</p>
+            <p className="card--title">{props.data.title}</p>
+            <p className="card--price"><span className='bold'>${props.data.price} </span>/ person</p>
         </section>
     )
 }
